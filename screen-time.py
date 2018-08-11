c214bb20cf50a4ac20b8c8b069c6e4ce3959168f
Kyle Corry <kylecorry31@gmail.com>
2018-08-11 08:09:01 -0400

    Simplify code

--- screen-time.py
+++ screen-time.py
@@ -10,70 +10,51 @@ THIS_WEEK_FILENAME = "week.txt"
 LAST_WEEK_FILENAME = "last-week.txt"
 
 
-def daemon(folder):
-    try:
-        os.makedirs(folder)
-    except OSError:
-        pass
+def daemon(folder, delay):
+    create_directory(folder)
 
-    create_file(os.path.join(folder, THIS_WEEK_FILENAME))
-    create_file(os.path.join(folder, LAST_WEEK_FILENAME))
+    this_week_path = os.path.join(folder, THIS_WEEK_FILENAME)
+    last_week_path = os.path.join(folder, LAST_WEEK_FILENAME)
 
-    week_file = open(os.path.join(folder, THIS_WEEK_FILENAME), "a+")
+    create_file(this_week_path)
+    create_file(last_week_path)
 
-    delay = 1
+    week_file = open(this_week_path, "r")
 
     time.sleep(delay)
 
     last_updated = get_time()
 
     if week_file:
-        # Read the first line of the week file and then reset
+        # Read the first line of the week file
         file_time = week_file.readline()
-        week_file.seek(0)
+        week_file.close()
 
         if file_time:
             file_time = int(file_time)
 
             if not on_same_week(file_time, last_updated):
                 # Save week file to archive
-                archive_file = open(os.path.join(folder, LAST_WEEK_FILENAME), "w")
-                archive_file.write(week_file.read())
-                archive_file.close()
+                copy_file(this_week_path, last_week_path)
 
                 # Erase week file contents
-                week_file.close()
-                week_file = open(os.path.join(folder, THIS_WEEK_FILENAME), "w")
-                week_file.close()
-
-        week_file.close()
+                erase_file_contents(this_week_path)
 
     while True:
-
-        week_file = open(os.path.join(folder, THIS_WEEK_FILENAME), "a")
         current_time = get_time()
-        week_file.write(str(current_time) + "\n")
-        week_file.close()
+        append_file(this_week_path, str(current_time) + "\n")
 
         if not on_same_week(current_time, last_updated):
-            week_file = open(os.path.join(folder, THIS_WEEK_FILENAME), "r")
             # Save week file to archive
-            archive_file = open(os.path.join(folder, LAST_WEEK_FILENAME), "w")
-            archive_file.write(week_file.read())
-            archive_file.close()
+            copy_file(this_week_path, last_week_path)
 
             # Erase week file contents
-            week_file = open(os.path.join(folder, THIS_WEEK_FILENAME), "w")
-            week_file.close()
+            erase_file_contents(this_week_path)
 
         time.sleep(delay)
 
 
-def create_file(file_path):
-    f = open(file_path, "a+")
-    f.close()
-
-
+# Date-Time Utils
 def get_time():
     return int(time.time())
 
@@ -95,9 +76,48 @@ def on_same_week(time1, time2):
     return t1.date().isocalendar()[1] == t2.date().isocalendar()[1]
 
 
+# FILE UTILS
+def create_file(file_path):
+    try:
+        f = open(file_path, "a+")
+        f.close()
+    except Exception:
+        pass
+
+
+def create_directory(path):
+    try:
+        os.makedirs(path)
+    except OSError:
+        pass
+
+
+def copy_file(src_path, dest_path):
+    src_file = open(src_path, "r")
+    dest_file = open(dest_path, "w+")
+
+    dest_file.write(src_file.read())
+
+    dest_file.close()
+    src_file.close()
+
+
+def erase_file_contents(path):
+    f = open(path, "w+")
+    f.close()
+
+
+def append_file(path, text):
+    f = open(path, "a+")
+    f.write(text)
+    f.close()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--directory", "-d",
                         help="The directory to write files to.", type=str, default=FOLDER)
+    parser.add_argument("--interval", "-i",
+                        help="The recording interval time in seconds. Defaults to 1 second.", type=float, default=1)
     args = parser.parse_args()
-    daemon(args.directory)
+    daemon(args.directory, args.interval)
